3416a80237efc0e39e4e3405e995d696b38b734c
Esau <esauhp97@gmail.com>
2021-11-29 10:47:30 +0100

    First Commit

---
+++ src/main/java/com/everis/bootcamp/productservice/ProductConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.everis.bootcamp.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductConfigServerApplication.class, args);
+	}
+
+}
